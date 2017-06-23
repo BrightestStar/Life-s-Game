94d7d1908e932154f8736b63933bce230fce3a68
lucas <d2194499406@gmail.com>
2017-06-23 16:28:52 +0800

    implement comment fuction

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,29 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+
+  def edit
+    @objective = Objective.find(params[:objective_id])
+    @post = Post.find(params[:id])
+    if current_user != @objective.user
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @objective = Objective.find(params[:objective_id])
+    @post = Post.find(params[:id])
+
+    if current_user != @objective.user
+      redirect_to root_path
+    end
+
+    if @post.update(post_params)
+      redirect_to objective_posts_path(@post)
+    else
+      render :edit
+    end
+  end
+
   def index
     @objective = Objective.find(params[:objective_id])
     @posts = @objective.posts
@@ -18,6 +42,18 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to objective_path(@objective)
     end
+  end
+
+  def destroy
+    @objective = Objective.find(params[:objective_id])
+    @post = Post.find(params[:id])
+
+    if current_user != @objective.user
+      redirect_to root_path
+    end
+
+    @post.destroy
+    redirect_to objective_path(@objective)
   end
 
   private
