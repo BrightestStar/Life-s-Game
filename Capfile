6e225b498f00881523104b77a903404e9b6c3589
lucas <d2194499406@gmail.com>
2017-06-30 10:08:47 +0800

    fix17

--- Capfile
+++ Capfile
@@ -4,6 +4,17 @@ require "capistrano/setup"
 # Include default deployment tasks
 require "capistrano/deploy"
 
+# Load the SCM plugin appropriate to your project:
+#
+# require "capistrano/scm/hg"
+# install_plugin Capistrano::SCM::Hg
+# or
+# require "capistrano/scm/svn"
+# install_plugin Capistrano::SCM::Svn
+# or
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
+
 # Include tasks from other gems included in your Gemfile
 #
 # For documentation on these, see for example:
@@ -23,11 +34,8 @@ require "capistrano/deploy"
 # require "capistrano/rails/migrations"
 # require "capistrano/passenger"
 
-require 'capistrano/rbenv'
-require 'rvm1/capistrano3'
-require "capistrano/bundler"
-require "capistrano/rails/assets"
-require "capistrano/rails/migrations"
+require 'capistrano/rails'
+require 'capistrano/passenger'
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -4,18 +4,13 @@ lock "3.8.0"
 set :application, "Life-s-Game"
 set :repo_url, "https://github.com/BrightestStar/Life-s-Game.git"
 
-set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, '2.0.0-p247'
-
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-set :rbenv_roles, :all # default value
-
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/apps/Life-s-Game"
+
+set :keep_releases, 5
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -29,10 +24,10 @@ set :deploy_to, "/home/apps/Life-s-Game"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+append :linked_files, 'config/database.yml', 'config/secrets.yml'
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 set :passenger_restart_with_touch, true
 # Default value for default_env is {}
